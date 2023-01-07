bccb40eacc9d9eac43d8e73125a12762e004910b
AmazeHere <amazeemail0@gmail.com>
2023-01-07 22:13:44 +0530

    debloat

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={Logo} className="logo react" alt="React logo" />
+        <a href="https://youtu.be/xvFZjo5PgG0" target="_blank">
+          <img src={Logo} className="logo"/>
         </a>
       </div>
-      <h1>I am from Marsrererer</h1>
+      <h1 className="hd1">I am from Marsrererer</h1>
       <div className="card">
-        <p>
+      <a href="https://www.ninjabot.cf/">
+        <p className= "p1">
           i know your mom's address
         </p>
+        </a>
       </div>
     </div>
   )
